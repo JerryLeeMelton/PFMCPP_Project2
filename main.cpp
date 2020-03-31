22178aa3b3d3fd26e02d5215f7f873efa4e2a9b5
Jerry Lee Melton <jerryleemelton@gmail.com>
2020-03-31 06:21:52 +0000

    Completed project.

--- main.cpp
+++ main.cpp
@@ -16,16 +16,14 @@ video: Chapter 2 - Part 3
  
  1) Write down the names of all of the primitives available in C++ (excluding wchar_t)
  put them here: 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+  
+bool 
+char
+int
+float
+double
+void
+
  
 2) for each primitive type, write out 3 variable declarations inside the variableDeclaration function.
     give each declaration an initial value
@@ -66,9 +64,47 @@ void variableDeclarations()
     //example:
     int number = 2; //declaration of a primitive named 'number' with an initial value of '2'
     
-    
+    bool isWritable = true;
+    bool playSound = false;
+    bool performOperation = true;
+
+    char separator = '_';
+    char gonGiveItToYa = 'X';
+    char special = 'k';
+
+    int failedAttempts = 1000000;
+    int unitsSold = 8248;
+    int numSamples = 44100;
+
+    float basePrice = 1.99f;
+    float percentCorrect = 0.70f;
+    float piRounded = 3.141593f;
+
+    double averagePerHour = 7.3453472756235;
+    double startingValue = 0.0;
+    double amplitudeModifier = 0.6789;
     
     ignoreUnused(number); //passing each variable declared to the ignoreUnused() function
+
+    ignoreUnused(isWritable);
+    ignoreUnused(playSound);
+    ignoreUnused(performOperation);
+
+    ignoreUnused(separator);
+    ignoreUnused(gonGiveItToYa);
+    ignoreUnused(special);
+
+    ignoreUnused(failedAttempts);
+    ignoreUnused(unitsSold);
+    ignoreUnused(numSamples);
+
+    ignoreUnused(basePrice);
+    ignoreUnused(percentCorrect);
+    ignoreUnused(piRounded);
+
+    ignoreUnused(averagePerHour);
+    ignoreUnused(startingValue);
+    ignoreUnused(amplitudeModifier);
 }
 /*
  10 functions
@@ -83,42 +119,90 @@ bool rentACar(int rentalDuration, int carType = 0)  //function declaration with 
 /*
  1)
  */
+bool canAfford(float moneyAvailable, float moneyNeeded = 29.99f)
+{
+    ignoreUnused(moneyAvailable, moneyNeeded);
+    return {};
+}
 
 /*
  2)
  */
+ int catsToFeed(int catsInAlley, int catsFostered = 6, int catsOwned = 4)
+ {
+     ignoreUnused(catsInAlley, catsFostered, catsOwned);
+     return{};
+ }
 
 /*
  3)
  */
+ int facesMelted(bool shreded, int peopleInAudience, int notesPlayed, int highKicksPerformed)
+ {
+     ignoreUnused(shreded, peopleInAudience, notesPlayed, highKicksPerformed);
+     return {};
+ }
 
 /*
  4)
  */
+ void bangHead(bool musicPlaying = true, float rockinFactor = 0.999456f)
+ {
+     ignoreUnused(musicPlaying, rockinFactor);
+ }
 
 /*
  5)
  */
+ bool yes(bool no = false)
+ {
+     ignoreUnused(no);
+     return{};
+ }
 
 /*
  6)
  */
+ double getBodyOdor(float temperature, double funkFactor, int daysSinceShower)
+ {
+     ignoreUnused(temperature, funkFactor, daysSinceShower);
+     return {};
+ }
 
 /*
  7)
  */
+ void sweetChildOMine(char o = 'o', int oh = 0)
+ {
+     ignoreUnused(o, oh);
+ }
 
 /*
  8)
  */
+ float xFactor(char x = 'X')
+ {
+     ignoreUnused(x);
+     return {};
+ }
 
 /*
  9)
  */
+int countSheep(int numSheep, float hoursUntilSleep) 
+{
+    ignoreUnused(numSheep, hoursUntilSleep);
+    return {};
+}
 
 /*
  10)
  */
+bool laughAtJoke(double awkwardPauseLength, bool isFunny = false)
+{
+    ignoreUnused(awkwardPauseLength, isFunny);
+    return {};
+}
 
 int main()
 {
@@ -126,27 +210,35 @@ int main()
     auto carRented = rentACar(6, 2); 
     
     //1)
-    
+    auto affordable = canAfford(123.50f, 29.99f);
     //2)
-    
+    auto numCats = catsToFeed(30, 6, 5);
     //3)
-    
+    auto meltyFaces = facesMelted(true, 50000, 1500000, 37);
     //4)
-    
+    bangHead(true, 0.04576f);
     //5)
-    
+    auto maybe = yes(false);
     //6)
-    
+    auto bodyOdor = getBodyOdor(106.0f, 0.99995, 16);
     //7)
-    
+    sweetChildOMine('o', 0);
     //8)
-    
+    auto whichFactor = xFactor('X');
     //9)
-    
+    auto sheepCounted = countSheep(75, 1.54f);
     //10)
-    
+    auto isLaughing = laughAtJoke(12.5, false);
     
     ignoreUnused(carRented);
+    ignoreUnused(affordable);
+    ignoreUnused(numCats);
+    ignoreUnused(meltyFaces);
+    ignoreUnused(maybe);
+    ignoreUnused(bodyOdor);
+    ignoreUnused(whichFactor);
+    ignoreUnused(sheepCounted);
+    ignoreUnused(isLaughing);
     std::cout << "good to go!" << std::endl;
     return 0;    
 }
